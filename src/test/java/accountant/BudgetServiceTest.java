789ac8bba787ab796e5c9b6b047c2f7852f03e90
AngusWu <anguswu@simbo.com.tw>
2019-07-27 18:03:40 +0800

    refactor test

--- src/test/java/accountant/BudgetServiceTest.java
+++ src/test/java/accountant/BudgetServiceTest.java
@@ -18,13 +18,21 @@ public class BudgetServiceTest {
 
     @Test
     public void test_single_whole_month() {
-        LocalDate start = LocalDate.of(2019, 1, 1);
-        LocalDate end = LocalDate.of(2019, 1, 31);
+        LocalDate start = prepareLocalDate(2019, 1, 1);
+        LocalDate end = prepareLocalDate(2019, 1, 31);
 
-        Budget budget = new Budget("201901", 3100);
+        Budget budget = createBudget("201901", 3100);
         doReturn(Collections.singletonList(budget)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 3100);
+    }
+
+    private Budget createBudget(String yearMonth, int amount) {
+        return new Budget(yearMonth, amount);
+    }
+
+    private LocalDate prepareLocalDate(int year, int month, int day) {
+        return LocalDate.of(year, month, day);
     }
 
     private void budgetShouldBe(LocalDate start, LocalDate end, int expected) {
@@ -33,10 +41,10 @@ public class BudgetServiceTest {
 
     @Test
     public void test_single_date_query() {
-        LocalDate start = LocalDate.of(2019, 1, 1);
-        LocalDate end = LocalDate.of(2019, 1, 1);
+        LocalDate start = prepareLocalDate(2019, 1, 1);
+        LocalDate end = prepareLocalDate(2019, 1, 1);
 
-        Budget budget = new Budget("201901", 310);
+        Budget budget = createBudget("201901", 310);
         doReturn(Collections.singletonList(budget)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 10);
@@ -44,10 +52,10 @@ public class BudgetServiceTest {
 
     @Test
     public void test_partial_month() {
-        LocalDate start = LocalDate.of(2019, 1, 1);
-        LocalDate end = LocalDate.of(2019, 1, 15);
+        LocalDate start = prepareLocalDate(2019, 1, 1);
+        LocalDate end = prepareLocalDate(2019, 1, 15);
 
-        Budget budget = new Budget("201901", 310);
+        Budget budget = createBudget("201901", 310);
         doReturn(Collections.singletonList(budget)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 150);
@@ -55,10 +63,10 @@ public class BudgetServiceTest {
 
     @Test
     public void test_invalid_range() {
-        LocalDate start = LocalDate.of(2019, 1, 15);
-        LocalDate end = LocalDate.of(2019, 1, 1);
+        LocalDate start = prepareLocalDate(2019, 1, 15);
+        LocalDate end = prepareLocalDate(2019, 1, 1);
 
-        Budget budget = new Budget("201901", 310);
+        Budget budget = createBudget("201901", 310);
         doReturn(Collections.singletonList(budget)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 0);
@@ -66,11 +74,11 @@ public class BudgetServiceTest {
 
     @Test
     public void test_cross_month() {
-        LocalDate start = LocalDate.of(2019, 1, 1);
-        LocalDate end = LocalDate.of(2019, 2, 28);
+        LocalDate start = prepareLocalDate(2019, 1, 1);
+        LocalDate end = prepareLocalDate(2019, 2, 28);
 
-        Budget budget = new Budget("201901", 310);
-        Budget budget1 = new Budget("201902", 280);
+        Budget budget = createBudget("201901", 310);
+        Budget budget1 = createBudget("201902", 280);
         doReturn(Arrays.asList(budget, budget1)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 590);
@@ -78,12 +86,12 @@ public class BudgetServiceTest {
 
     @Test
     public void test_cross_three_month() {
-        LocalDate start = LocalDate.of(2019, 1, 31);
-        LocalDate end = LocalDate.of(2019, 3, 3);
+        LocalDate start = prepareLocalDate(2019, 1, 31);
+        LocalDate end = prepareLocalDate(2019, 3, 3);
 
-        Budget budget = new Budget("201901", 310);
-        Budget budget1 = new Budget("201902", 280);
-        Budget budget2 = new Budget("201903", 310);
+        Budget budget = createBudget("201901", 310);
+        Budget budget1 = createBudget("201902", 280);
+        Budget budget2 = createBudget("201903", 310);
         doReturn(Arrays.asList(budget, budget1, budget2)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 320);
@@ -91,10 +99,10 @@ public class BudgetServiceTest {
 
     @Test
     public void test_no_budget() {
-        LocalDate start = LocalDate.of(2019, 1, 31);
-        LocalDate end = LocalDate.of(2019, 3, 3);
+        LocalDate start = prepareLocalDate(2019, 1, 31);
+        LocalDate end = prepareLocalDate(2019, 3, 3);
 
-        Budget budget = new Budget("201904", 300);
+        Budget budget = createBudget("201904", 300);
         doReturn(Arrays.asList(budget)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 0);
@@ -102,11 +110,11 @@ public class BudgetServiceTest {
 
     @Test
     public void test_cross_year() {
-        LocalDate start = LocalDate.of(2018, 12, 31);
-        LocalDate end = LocalDate.of(2019, 2, 1);
+        LocalDate start = prepareLocalDate(2018, 12, 31);
+        LocalDate end = prepareLocalDate(2019, 2, 1);
 
-        Budget budget = new Budget("201812", 310);
-        Budget budget1 = new Budget("201901", 310);
+        Budget budget = createBudget("201812", 310);
+        Budget budget1 = createBudget("201901", 310);
         doReturn(Arrays.asList(budget, budget1)).when(budgetRepo).getAll();
 
         budgetShouldBe(start, end, 320);
