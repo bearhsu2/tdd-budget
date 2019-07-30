5c4e60d7071c603cda9f7f8488aa502f36ae3f28
AngusWu <anguswu@simbo.com.tw>
2019-07-30 13:39:24 +0800

    return totalAmount

--- src/main/java/accountant/BudgetService.java
+++ src/main/java/accountant/BudgetService.java
@@ -40,9 +40,8 @@ public class BudgetService {
                 LocalDate middle = start.plusMonths(i);
                 totalAmount += calculateBudgetAverage(middle) * middle.lengthOfMonth();
             }
-            return totalAmount;
         }
-        return 0;
+        return totalAmount;
     }
 
     private Optional<Budget> getBudget(LocalDate start) {
