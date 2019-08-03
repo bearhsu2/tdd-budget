61a27c7446a94b0ee3d8599ff86930d34ef8aab1
AngusWu <anguswu@simbo.com.tw>
2019-08-03 23:14:02 +0800

    clean up BudgetService

--- src/main/java/accountant/BudgetService.java
+++ src/main/java/accountant/BudgetService.java
@@ -1,10 +1,6 @@
 package accountant;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-
-import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class BudgetService {
     private BudgetRepo budgetRepo;
@@ -21,26 +17,9 @@ public class BudgetService {
 
         double totalAmount = 0;
         Period period = new Period(start, end);
-        LocalDate currentDate = start;
-//        while (currentDate.isBefore(YearMonth.from(end).plusMonths(1).atDay(1))) {
-//            Optional<Budget> currentBudget = getBudget(currentDate);
-//
-//            if (currentBudget.isPresent()) {
-//                Budget budget = currentBudget.get();
-//
         for (Budget budget : budgetRepo.getAll()) {
             totalAmount += budget.getOverlappingAmount(period);
         }
-//            }
-//            currentDate = currentDate.plusMonths(1);
-//        }
         return totalAmount;
     }
-
-    private Optional<Budget> getBudget(LocalDate start) {
-        DateTimeFormatter formatter = ofPattern("yyyyMM");
-        return budgetRepo.getAll().stream()
-                         .filter(b -> b.getYearMonth().equals(start.format(formatter))).findFirst();
-    }
-
 }
